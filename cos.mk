95575ccfa9e60bad58959b96170f27426b9e0194
Egg Oxygen <ken442266@gmail.com>
2017-04-29 15:16:46 +0800

    COSMIC-ify 	*Change To COS 	*Remove Depend 	*Add VendorSetup.sh

--- cos.mk
+++ cos.mk
@@ -13,11 +13,12 @@
 # limitations under the License.
 
 $(call inherit-product, device/htc/pme/full_pme.mk)
-$(call inherit-product, vendor/candy/config/common_full_phone.mk)
+$(call inherit-product, vendor/cos/common.mk)
 
 ## Device identifier. This must come after all inclusions
+COS_RELEASE := true
 PRODUCT_DEVICE := pme
-PRODUCT_NAME := candy_pme
+PRODUCT_NAME := cos_pme
 PRODUCT_BRAND := HTC
 PRODUCT_MODEL := HTC 10
 PRODUCT_MANUFACTURER := HTC
---
+++ vendorsetup.sh
@@ -0,0 +1,3 @@
+add_lunch_combo cos_pme-user
+add_lunch_combo cos_pme-userdebug
+add_lunch_combo cos_pme-eng
